479204137ddaee2f2100d1cffb79065be2f747f1
zhaochun-vmware <126732652+zhaochun-vmware@users.noreply.github.com>
2023-07-14 11:55:28 +0800

    Create ClientWorkerPutSSL.java

---
+++ gemfire-client/ClientWorkerPutSSL.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import org.apache.geode.cache.client.ClientCacheFactory;
+import org.apache.geode.cache.client.ClientCache;
+import org.apache.geode.cache.Region;
+import org.apache.geode.cache.InterestResultPolicy;
+
+
+
+public class ClientWorkerPutSSL {
+
+  public static final String EXAMPLE_REGION_NAME = "exampleRegion";
+
+  public static void main(String[] args) throws Exception {
+
+    System.out.println("Connecting to the distributed system and creating the cache.");
+
+    ClientCache cache = new ClientCacheFactory()
+        .set("name", "ClientWorkerPut")
+        .set("cache-xml-file", "ClientProducer.xml")
+        .set("log-level", "config")
+        .set("log-file", "producer.log")
+	.set("ssl-enabled-components", "locator")
+	.set("ssl-keystore","/root/gemfire.keystore")
+	.set("ssl-keystore-password", "password")
+	.set("ssl-truststore", "/root/gemfire.truststore")
+	.set("ssl-truststore-password", "password")
+	.addPoolLocator("10.0.0.4", 10334)
+        .create();
+
+
+    // Get the exampleRegion
+    Region<String, String> exampleRegion = cache.getRegion(EXAMPLE_REGION_NAME);
+
+    for (int count = 0; count < 1000; count++) {
+      String key = "4key" + count;
+      exampleRegion.put(key,key);
+    }
+
+    // Close the cache and disconnect from GemFire distributed system
+    System.out.println("Closing the cache and disconnecting.");
+    cache.close();
+  }
+}
