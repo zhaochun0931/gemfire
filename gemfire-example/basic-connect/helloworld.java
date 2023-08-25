955f81421cdaec9f49096547102e708245483f9d
zhaochun-vmware <126732652+zhaochun-vmware@users.noreply.github.com>
2023-08-25 12:20:36 +0800

    Update helloworld.java

--- gemfire-example/basic-connect/helloworld.java
+++ gemfire-example/basic-connect/helloworld.java
@@ -1,1 +1,35 @@
-xxx
+import java.util.Map;
+import org.apache.geode.cache.Region;
+import org.apache.geode.cache.client.*;
+
+
+
+public class helloworld 
+{
+    public static void main( String[] args ) throws Exception
+    {
+
+//        System.out.println( "Hello World!" );
+
+        ClientCache cache = new ClientCacheFactory()
+                .addPoolLocator("localhost", 10334)
+                .create();
+        Region<String, String> region = cache
+                .<String, String>createClientRegionFactory(ClientRegionShortcut.CACHING_PROXY)
+                .create("hello");
+
+        region.put("1", "Hello");
+        region.put("2", "World");
+
+        for (Map.Entry<String, String>  entry : region.entrySet()) {
+            System.out.format("key = %s, value = %s\n", entry.getKey(), entry.getValue());
+        }
+        cache.close();
+
+
+
+
+
+
+    }
+}
