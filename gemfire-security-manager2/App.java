cb2103f241f6641a8e1cf834b0d3cf0bfcca5391
zhaochun-vmware <126732652+zhaochun-vmware@users.noreply.github.com>
2023-09-30 20:37:23 +0800

    Update App.java

--- gemfire-security-manager2/App.java
+++ gemfire-security-manager2/App.java
@@ -1,9 +1,17 @@
 //package com.mycompany.app;
 
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Properties;
 
 import org.apache.geode.security.AuthenticationFailedException;
+import org.apache.geode.security.ResourcePermission;
 import org.apache.geode.security.SecurityManager;
+
+
 
 
 /**
@@ -14,17 +22,17 @@ public class App implements SecurityManager
 {
 
     @Override
-
-    public Object authenticate(Properties credentials) throws AuthenticationFailedException{
-        Boolean isAuthenticated = false;
-        String username = credentials.getProperty("security-username");
-        String password = credentials.getProperty("security-password");
-
-        if ("myuser".equals(username) && "mypassword".equals(password) ) {
-            isAuthenticated = true;
-        } else{
-            throw new AuthenticationFailedException("Wrong username/password");
-        }
-        return isAuthenticated;
+    public Object authenticate(Properties credentials) throws AuthenticationFailedException {
+        return "USER";
     }
+
+
+    @Override
+    public boolean authorize(Object principal, ResourcePermission permission) {
+        return true;
+    }
+
+
+
+
 }
