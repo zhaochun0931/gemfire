a9405de75b2dfcaa345022721f9ae59ec823fbb2
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2024-04-02 22:50:06 +0800

    Create App.java

---
+++ gemfire-java-client-maven-2/App.java
@@ -0,0 +1,20 @@
+import org.apache.geode.cache.Region;
+import org.apache.geode.cache.client.ClientCache;
+import org.apache.geode.cache.client.ClientCacheFactory;
+
+public class App {
+    public static void main(String[] args) {
+        ClientCache cache = new ClientCacheFactory()
+                .set("cache-xml-file", "/root/my-app/client-cache.xml")
+                .create();
+
+        Region<String, String> region = cache.getRegion("exampleRegion");
+
+        region.put("key1", "value1");
+        region.put("key2", "value2");
+
+        System.out.println("Entries written to GemFire server successfully.");
+
+        cache.close();
+    }
+}
