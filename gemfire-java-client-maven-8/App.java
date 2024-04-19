f339d7e3dfb37a2e923f603a6e3094952cbbbaae
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2024-04-19 12:35:20 +0800

    Update App.java

--- gemfire-java-client-maven-8/App.java
+++ gemfire-java-client-maven-8/App.java
@@ -1,1 +1,25 @@
-test
+import org.apache.geode.cache.Region;
+import org.apache.geode.cache.client.*;
+import java.util.Date;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+        ClientCache cache = new ClientCacheFactory().addPoolLocator("localhost", 10334).create();
+        Region<String, String> region = cache.<String, String>createClientRegionFactory(ClientRegionShortcut.CACHING_PROXY).create("exampleRegion");
+
+        System.out.println("Putting 10000 entries");
+        System.out.println("Start: " + new Date());
+
+        for (int i = 1; i < 10001; i++){
+            region.put("" + i, " " + i + " hello world");
+
+        }
+
+        System.out.println("Finish:" + new Date());
+
+	      cache.close();
+
+    }
+}
