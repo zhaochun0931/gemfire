75130bdf8c99fab925397c644314a4850c558dd1
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2024-12-19 21:36:33 +0800

    Update and rename App.java to gemfireBasic.java

--- gemfire-maven/gemfireBasic.java
+++ gemfire-maven/gemfireBasic.java
@@ -4,7 +4,7 @@ import org.apache.geode.cache.client.ClientCacheFactory;
 import org.apache.geode.cache.client.ClientRegionShortcut;
 
 
-public class App
+public class gemfireBasic
 {
     public static void main( String[] args )
     {
