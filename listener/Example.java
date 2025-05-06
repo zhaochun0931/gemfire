14eeeebb96d4250bc161f8f8ed4c60f9b7618133
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2025-05-06 20:41:38 +0800

    Update Example.java

--- listener/Example.java
+++ listener/Example.java
@@ -6,7 +6,7 @@ import org.apache.geode.cache.client.ClientRegionShortcut;
 
 public class Example {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // Connect to the locator using default port 10334
         ClientCache cache = new ClientCacheFactory()
             .addPoolLocator("127.0.0.1", 10334)
@@ -22,12 +22,13 @@ public class Example {
         // Put 10 entries into the region
         for (int i = 1; i <= 10; i++) {
             region.put(i, "hello-" + i);
+            Thread.sleep(1000);
         }
 
         // remove 10 entries from the region
         for (int i = 1; i <= 10; i++) {
             region.remove(i);
-            Thread.sleep(1000);
+            Thread.sleep(2000);
 
         }
         
