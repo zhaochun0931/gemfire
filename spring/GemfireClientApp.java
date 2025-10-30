32c3107fd8df61b0aaf11e18e626ace81f8ae881
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2025-10-30 14:54:10 +0800

    Update GemfireClientApp.java

--- spring/GemfireClientApp.java
+++ spring/GemfireClientApp.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Bean;
 public class GemfireClientApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(HelloworldApplication.class, args);
+        SpringApplication.run(GemfireClientApp.class, args);
     }
 
     @Bean
