d2646086923e21da99de1595dff3552738bb265a
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2025-10-30 21:03:10 +0800

    Update GemfireClient.java

--- client/GemfireClient.java
+++ client/GemfireClient.java
@@ -5,15 +5,20 @@ import org.apache.geode.cache.client.ClientCacheFactory;
 public class GemfireClient {
     public static void main(String[] args) {
         ClientCache cache = new ClientCacheFactory()
-                .set("cache-xml-file", "/root/client-cache.xml")
+                .set("cache-xml-file", "/usr/src/mymaven/my-app/cache.xml")
                 .create();
 
         Region<String, String> region = cache.getRegion("exampleRegion");
 
-        region.put("key1", "value1");
-        region.put("key2", "value2");
 
-        System.out.println("Entries written to GemFire server successfully.");
+	    for (int i = 1; i <= 1000; i++) {
+            String key = "key" + i;
+            String value = "value" + i;
+            region.put(key, value);
+        }
+
+        System.out.println("✅ Inserted 1000 entries into the region!");
+
 
         cache.close();
     }
