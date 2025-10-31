9279ae94ad1c2f7b649636a8f003403a8710298e
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2025-10-31 14:22:51 +0800

    Create GemfireClient.java

---
+++ client-gradle/GemfireClient.java
@@ -0,0 +1,25 @@
+import org.apache.geode.cache.Region;
+import org.apache.geode.cache.client.ClientCache;
+import org.apache.geode.cache.client.ClientCacheFactory;
+
+public class GemfireClient {
+    public static void main(String[] args) {
+        ClientCache cache = new ClientCacheFactory()
+                .set("cache-xml-file", "/home/gradle/project/gf-client/app/src/main/java/org/example/cache.xml")
+                .create();
+
+        Region<String, String> region = cache.getRegion("exampleRegion");
+
+
+	    for (int i = 1; i <= 1000; i++) {
+            String key = "key" + i;
+            String value = "value" + i;
+            region.put(key, value);
+        }
+
+        System.out.println("✅ Inserted 1000 entries into the region!");
+
+
+        cache.close();
+    }
+}
